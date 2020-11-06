772245443e39795b511565a5eda9db67a6758b81
Liam Woodleigh-Hardinge <liam.woodleigh@gmail.com>
2020-11-06 15:49:48 +0100

    Readme tables content update

--- utils/_readme_tables.py
+++ utils/_readme_tables.py
@@ -16,53 +16,26 @@ from jinja2 import Template
 
 tables = {
     "Switches 🎚": [
-        ["TP-Link HS110", 3, 70.84],
-        ["Xiaomi Aqara Magic Cube", 1, 11.08],
-        ["Xiaomi Aqara Single Button", 4, 57.38],
-        ["Philips Hue Dimmer switch", 2, 33.22],
+        ["Philips Hue Dimmer switch", 3, 50.07],
     ],
     "Sensors 🌡": [
-        ["Xiaomi Aqara Door Sensor", 4, 32.71],
-        ["Xiaomi Aqara Temperature Sensor", 5, 45.74],
-        ["Xiaomi Aqara Motion Sensor", 6 + 4, 61.13 + 36.72],
-        ["Xiaomi Aqara Vibration Sensor", 1, 11.59],
-        ["Xiaomi Mi Flora", 3, 46.63],
+        ["Xiaomi Aqara Door Sensor", 2, 16.98],
+        ["Philips Hue Motion, Temperature & Humidity Sensor", 1, 39.95],
     ],
-    "Vacuum 🧹": [["Xiaomi Mi Roborock S5", 1, 294.31]],
+    "Vacuum 🧹": [["Xiaomi Mi Roborock S5 Max", 1, 383.92]],
     "Media player 📺🔈": [
-        ["KEF LS50 Wireless speakers", 1, "nan"],
-        ["KEF LSX speakers", 1, "nan"],
-        ["LG OLED 55 C9", 1, "nan"],
+        ["Philips The One 58", 1, "nan"],
+        ["Google Home Hub", 2, 119.98],
     ],
-    "Lights 💡": [
-        ["Philips Hue E27 White and Color", 12, 266.70],
-        ["Philips Hue E14 White and Color", 2, 69.77],
-        ["Philips Hue GU10 Ambient White", 5, 92.70],
-        ["Philips Hue Go", 1, 57.05],
-        ["Philips Hue LED strip 2m", 2, 86.39],
-        ["Philips Hue LED strip 1m", 1, 13.84],
+    "Lights 💡": [  # TODO: Prices not correct
+        ["Philips Hue E27 White and Color", 5, 217.88],
+        ["Philips Hue E14 White and Color", 3, 131.98],
+        ["Philips Hue GU10 White Ambience ", 3, 63],
+        ["Philips Hue GU10 White and Color", 3, 119.25],
     ],
     "Hubs 🌎": [["ConBee II", 1, 39.95]],
-    "Server 🖥": [
-        ["Intel NUC Kit NUC8i3BEH", 1, 278.30],
-        ["8 GB Crucial CT8G4SFS824A DDR4", 2, 51.04],
-        ["Samsung 970 EVO M.2 500GB", 1, 94.99],
-        [
-            "Raspberry Pi 4, 4GB RAM (connected to power/gas meter and connected over MQTT to main HA instance)",
-            1,
-            70.90,
-        ],
-        ["Raspberry Pi 4 FLIRC Case", 1, 25.85],
-        ["SanDisk Ultra microSDHC Memory Card 32GB", 1, 6.99],
-    ],
     "Device tracker 🔍": [
-        ["iPhone X with the iOS app", 1, "nan"],
-        ["iPhone SE2 with the iOS app", 1, "nan"],
-    ],
-    "Other": [
-        ["PlayStation Eye Webcam and Microphone array", 1, 14.95],
-        ["DSMR - Slimme Meter kabel", 1, 19.95],
-        ["Nodo-Shop – OpenTherm Gateway (OTGW) with NodeMCU", 1, 48.81],
+        ["iPhone XS with the iOS app", 1, "nan"],
     ],
 }
 
